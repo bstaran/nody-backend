aa6010fc8b34e0e7f90468c74a87070273294ce6
ByeongGyu An <devafox62@gmail.com>
2025-06-20 16:26:43 +0900

    chore(project): .gitignore 및 문서 템플릿 업데이트

--- src/main/java/org/nodystudio/nodybackend/config/SecurityConfig.java
+++ src/main/java/org/nodystudio/nodybackend/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.nodystudio.nodybackend.security.handler.CustomAccessDeniedHandler;
 import org.nodystudio.nodybackend.security.handler.CustomAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
