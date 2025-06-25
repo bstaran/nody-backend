ccc8adaadc8ac62f6d240166c3f22b31f3d781cc
ByeongGyu An <devafox62@gmail.com>
2025-06-25 18:35:43 +0900

    chore(config): 스케줄링 기능 활성화
    - @EnableScheduling 어노테이션 추가
    - 배치 작업 실행을 위한 스케줄러 활성화

--- src/main/java/org/nodystudio/nodybackend/NodyBackendApplication.java
+++ src/main/java/org/nodystudio/nodybackend/NodyBackendApplication.java
@@ -2,8 +2,10 @@ package org.nodystudio.nodybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NodyBackendApplication {
 
     public static void main(String[] args) {
